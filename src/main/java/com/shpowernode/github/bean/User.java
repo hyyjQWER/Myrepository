98f3a3b87af51bc161f781295eedb2d368431bcf
HY <2420828743@qq.com>
2020-11-14 16:52:16 +0800

    第一次提交项目

---
+++ src/main/java/com/shpowernode/github/bean/User.java
@@ -0,0 +1,16 @@
+package com.shpowernode.github.bean;
+
+/**
+ * @ProjectName: github
+ * @Package: com.shpowernode.github.bean
+ * @Description: java类作用描述
+ * @Author: 黄宇
+ * @CreateDate: 2020/11/14 16:47
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class User {
+    private String name ;
+    private int age ;
+}
